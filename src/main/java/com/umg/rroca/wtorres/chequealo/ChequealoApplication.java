1d57f277b0aa6774163ea548202349d30c84076b
hroca <rroca@praga.ws>
2021-10-06 00:28:49 -0600

    First commit

---
+++ src/main/java/com/umg/rroca/wtorres/chequealo/ChequealoApplication.java
@@ -0,0 +1,13 @@
+package com.umg.rroca.wtorres.chequealo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequealoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChequealoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/rroca/wtorres/chequealo/ChequealoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.rroca.wtorres.chequealo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChequealoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
